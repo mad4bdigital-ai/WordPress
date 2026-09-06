8b750100b0edb2b5b297865ea9a5f0db99b0ba00
MAD4B <mad4b.digital@gmail.com>
2026-09-06 20:49:04 +0300

    feat(mcp): load governance admin console

--- wp-content/plugins/mad4b-site-control-plane/mad4b-site-control-plane.php
+++ wp-content/plugins/mad4b-site-control-plane/mad4b-site-control-plane.php
@@ -47,6 +47,7 @@ require_once MAD4B_SCP_DIR . 'includes/adapters/class-mad4b-scp-litespeed-adapte
 require_once MAD4B_SCP_DIR . 'includes/class-mad4b-scp-adapter-registry.php';
 require_once MAD4B_SCP_DIR . 'includes/class-mad4b-scp-servers.php';
 require_once MAD4B_SCP_DIR . 'includes/class-mad4b-scp-governance-abilities.php';
+require_once MAD4B_SCP_DIR . 'includes/class-mad4b-scp-admin-ui.php';
 require_once MAD4B_SCP_DIR . 'includes/class-mad4b-scp-plugin.php';
 
 register_activation_hook( __FILE__, array( 'MAD4B_SCP_Plugin', 'activate' ) );
