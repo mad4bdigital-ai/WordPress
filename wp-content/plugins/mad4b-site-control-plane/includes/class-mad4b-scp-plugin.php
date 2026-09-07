63dfb5e54ede721e907ddfdc0571d1c008d60e54
MAD4B <mad4b.digital@gmail.com>
2026-09-08 01:54:24 +0300

    feat(oauth): boot standalone local authority

--- wp-content/plugins/mad4b-site-control-plane/includes/class-mad4b-scp-plugin.php
+++ wp-content/plugins/mad4b-site-control-plane/includes/class-mad4b-scp-plugin.php
@@ -22,6 +22,7 @@ final class MAD4B_SCP_Plugin {
 		self::$booted = true;
 
 		MAD4B_SCP_MCP_Provider_Isolation::boot();
+		MAD4B_SCP_Local_OAuth_Server::boot();
 		MAD4B_SCP_OAuth_Resource_Bridge::boot();
 		MAD4B_SCP_OAuth_Subject_Gate::boot();
 		MAD4B_SCP_MCP_Client_Compatibility::boot();
