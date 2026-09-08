ddb92f9871c737cc397ee96b1acd7371ee823ab9
MAD4B <mad4b.digital@gmail.com>
2026-09-08 16:28:42 +0300

    feat(chatgpt): load ChatGPT connection UI

--- wp-content/plugins/mad4b-site-control-plane/mad4b-site-control-plane.php
+++ wp-content/plugins/mad4b-site-control-plane/mad4b-site-control-plane.php
@@ -71,6 +71,7 @@ require_once MAD4B_SCP_DIR . 'includes/class-mad4b-scp-connection-ability.php';
 require_once MAD4B_SCP_DIR . 'includes/class-mad4b-scp-admin-experience.php';
 require_once MAD4B_SCP_DIR . 'includes/class-mad4b-scp-admin-ui.php';
 require_once MAD4B_SCP_DIR . 'includes/class-mad4b-scp-connection-admin-ui.php';
+require_once MAD4B_SCP_DIR . 'includes/class-mad4b-scp-chatgpt-connection-admin-ui.php';
 require_once MAD4B_SCP_DIR . 'includes/class-mad4b-scp-adapter-coverage-admin-ui.php';
 require_once MAD4B_SCP_DIR . 'includes/class-mad4b-scp-runtime-components-admin-ui.php';
 require_once MAD4B_SCP_DIR . 'includes/class-mad4b-scp-plugin.php';
