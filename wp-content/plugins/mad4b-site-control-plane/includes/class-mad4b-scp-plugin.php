6bcbb174ffd46b3d7ec77c68999f533a8ac7d2ba
MAD4B <mad4b.digital@gmail.com>
2026-09-08 16:04:58 +0300

    feat(oauth): boot bundled browser canary

--- wp-content/plugins/mad4b-site-control-plane/includes/class-mad4b-scp-plugin.php
+++ wp-content/plugins/mad4b-site-control-plane/includes/class-mad4b-scp-plugin.php
@@ -52,6 +52,7 @@ final class MAD4B_SCP_Plugin {
 		MAD4B_SCP_Connection_Admin_UI::boot();
 		MAD4B_SCP_Adapter_Coverage_Admin_UI::boot();
 		MAD4B_SCP_Runtime_Components_Admin_UI::boot();
+		MAD4B_SCP_Local_OAuth_Browser_Canary::boot();
 
 		if ( ! function_exists( 'wp_register_ability' ) ) {
 			add_action( 'admin_notices', array( __CLASS__, 'abilities_notice' ) );
