6d5391caa113f9d243d180f9929dcfd1dafc8792
MAD4B <mad4b.digital@gmail.com>
2026-09-08 14:07:43 +0300

    fix(oauth): boot request context guard before subject policy

--- wp-content/plugins/mad4b-site-control-plane/includes/class-mad4b-scp-plugin.php
+++ wp-content/plugins/mad4b-site-control-plane/includes/class-mad4b-scp-plugin.php
@@ -26,6 +26,7 @@ final class MAD4B_SCP_Plugin {
 		MAD4B_SCP_Local_OAuth_Init_Lock::boot();
 		MAD4B_SCP_Local_OAuth_Loopback_Guard::boot();
 		MAD4B_SCP_Local_OAuth_Server::boot();
+		MAD4B_SCP_OAuth_Request_Context_Guard::boot();
 		MAD4B_SCP_OAuth_Resource_Bridge::boot();
 		MAD4B_SCP_OAuth_Subject_Gate::boot();
 		MAD4B_SCP_MCP_Client_Compatibility::boot();
