f19da2f98e382c55e4dbc57d0087bb3af28b01a2
MAD4B <mad4b.digital@gmail.com>
2026-09-09 17:13:22 +0300

    fix(skills): intercept supporting-file writes before admin render

--- wp-content/plugins/mad4b-site-control-plane/includes/class-mad4b-scp-plugin.php
+++ wp-content/plugins/mad4b-site-control-plane/includes/class-mad4b-scp-plugin.php
@@ -58,6 +58,7 @@ final class MAD4B_SCP_Plugin {
 		MAD4B_SCP_ChatGPT_Connection_Admin_UI::boot();
 		MAD4B_SCP_Adapter_Coverage_Admin_UI::boot();
 		MAD4B_SCP_Runtime_Components_Admin_UI::boot();
+		MAD4B_SCP_Skill_Resource_Writer::boot();
 		MAD4B_SCP_Skills_Admin_UI::boot();
 		MAD4B_SCP_Local_OAuth_Browser_Canary::boot();
 
