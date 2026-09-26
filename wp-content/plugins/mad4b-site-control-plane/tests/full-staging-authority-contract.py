b055f4bd09049f6e6a10b171becc3ca4ebf3dab2
MAD4B <mad4b.digital@gmail.com>
2026-09-26 13:21:16 +0300

    test(authority): admit bounded enrollment dispatcher

--- wp-content/plugins/mad4b-site-control-plane/tests/full-staging-authority-contract.py
+++ wp-content/plugins/mad4b-site-control-plane/tests/full-staging-authority-contract.py
@@ -121,7 +121,7 @@ assert "MAD4B_SCP_Staging_Write_Grant_Reconciliation::chatgpt_step_up_tools()" i
 assert "MAD4B_SCP_Full_Staging_Authority::chatgpt_read_tools()" in chatgpt_tools
 assert "MAD4B_SCP_Full_Staging_Authority::chatgpt_step_up_tools()" in chatgpt_tools
 assert "$step_up = array_merge( $narrow_step_up, $full_step_up )" in chatgpt_tools
-assert "$direct_mutation_transport = array_merge( array( 'mad4b/write-execute' ), $step_up )" in chatgpt_tools
+assert "$direct_mutation_transport = array_merge( array( 'mad4b/write-execute', 'mad4b/enrollment-execute' ), $step_up )" in chatgpt_tools
 for low_level in [
     "'mad4b/site-profile-feature-reenroll'",
     "'mad4b/site-profile-write-enable'",
