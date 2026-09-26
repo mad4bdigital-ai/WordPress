01f94ac6becb92f45adde85eee5511f7e9f13455
MAD4B <mad4b.digital@gmail.com>
2026-09-26 14:08:27 +0300

    fix(oauth): advertise step-up for bounded enrollment dispatch

--- wp-content/plugins/mad4b-site-control-plane/includes/class-mad4b-scp-oauth-resource-bridge.php
+++ wp-content/plugins/mad4b-site-control-plane/includes/class-mad4b-scp-oauth-resource-bridge.php
@@ -176,9 +176,19 @@ final class MAD4B_SCP_OAuth_Resource_Bridge {
 	}
 
 	public static function authority_step_up_scope_available() {
-		if ( ! class_exists( 'MAD4B_SCP_Full_Staging_Authority' ) || ! method_exists( 'MAD4B_SCP_Full_Staging_Authority', 'chatgpt_step_up_tools' ) ) return false;
-		$tools = MAD4B_SCP_Full_Staging_Authority::chatgpt_step_up_tools();
-		return is_array( $tools ) && in_array( MAD4B_SCP_Full_Staging_Authority::APPLY_ABILITY, $tools, true );
+		$full_authority = false;
+		if ( class_exists( 'MAD4B_SCP_Full_Staging_Authority' ) && method_exists( 'MAD4B_SCP_Full_Staging_Authority', 'chatgpt_step_up_tools' ) ) {
+			$tools = MAD4B_SCP_Full_Staging_Authority::chatgpt_step_up_tools();
+			$full_authority = is_array( $tools ) && in_array( MAD4B_SCP_Full_Staging_Authority::APPLY_ABILITY, $tools, true );
+		}
+		$enrollment_dispatch = false;
+		if ( class_exists( 'MAD4B_SCP_Servers' ) && method_exists( 'MAD4B_SCP_Servers', 'chatgpt_tools' ) ) {
+			$tools = MAD4B_SCP_Servers::chatgpt_tools();
+			$enrollment_dispatch = is_array( $tools ) && in_array( 'mad4b/enrollment-execute', $tools, true );
+		} elseif ( function_exists( 'wp_has_ability' ) ) {
+			$enrollment_dispatch = wp_has_ability( 'mad4b/enrollment-execute' );
+		}
+		return $full_authority || $enrollment_dispatch;
 	}
 
 	public static function resource_identifier( $server_id = 'mad4b-chatgpt' ) {
